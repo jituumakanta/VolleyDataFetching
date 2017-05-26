61428c5836f537f0e1509293a66797b33b1161ac
umakant <jitu.umakanta@gmail.com>
2017-05-26 15:21:46 +0530

    1

--- app/src/main/java/com/example/wrapme09/volleydatafetching/MainActivity.java
+++ app/src/main/java/com/example/wrapme09/volleydatafetching/MainActivity.java
@@ -12,38 +12,58 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList e = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getofferNews();
+
     }
 
+    int i;
 
     private JsonObjectRequest getofferNews() {
 
-        String url = "http://quliver.com/wrapme/get_productname.php";
-        final JsonObjectRequest jsObjRequest5 = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        // String url = "http://quliver.com/wrapme/get_productname.php";
+        JSONObject jj;
+        //   final ArrayList e = new ArrayList();
+
+
+        Response.Listener<JSONObject> d = new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
 
                 System.out.println(response);
+                e.add(response);
+                i = 10;
+
             }
-        }, new Response.ErrorListener() {
+        };
+
+
+        System.out.println(i);
+        Response.ErrorListener s = new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
             }
-        });
+        };
 
+
+        JsonObjectRequest jsObjRequest5 = new JsonObjectRequest(Request.Method.GET, "http://quliver.com/wrapme/get_productname.php", null, d, s);
+        JsonObjectRequest jsObjRequest56 = new JsonObjectRequest(Request.Method.GET, "http://quliver.com/wrapme/get_productname.php", null, d, s);
         MySingleton.getInstance(this).addToRequestQueue(jsObjRequest5);
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest56);
+        // System.out.println("sssssssssss" + e);
         return jsObjRequest5;
 
     }
-
 
 
 
